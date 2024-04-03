6cafbdadf93a01cd6957667cbcc7915e86dc24b0
Utkarsh Saxena <utkarshsaxena360@gmail.com>
2024-04-03 14:51:34 +0530

    Add doctor model

--- src/model-exports.ts
+++ src/model-exports.ts
@@ -1,4 +1,6 @@
 import { getModelForClass } from "@typegoose/typegoose";
 import { ClinicData } from "./ClinicData/model";
+import { Doctor } from "./doctor/model";
 
 export const ClinicDataModel = getModelForClass(ClinicData);
+export const DoctorModel = getModelForClass(Doctor);
